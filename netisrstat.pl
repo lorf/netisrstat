aa47c96cfb2d0efe8246d2f29857267fd66dab5d
Dmitry Frolov <dmitry.frolov@gmail.com>
2013-12-25 11:51:32 +0700

    Add ISR CPU load display

--- netisrstat.pl
+++ netisrstat.pl
@@ -3,7 +3,32 @@
 use strict;
 use warnings;
 
-sub collect {
+sub collect_top {
+    my $interval = shift;
+    open TOP, "top -SHb -d 2 -s $interval |" or die "Can't spawn top: $!";
+    my $got_stats = 0;
+    my $timestamp = time;
+    my %top;
+    while (<TOP>) {
+        chomp;
+        if (/^\s*PID\s/) {
+            $got_stats++;
+        } elsif($got_stats == 2) {
+            s/^\s*//;
+            my @d = split /\s+/, $_, 11;
+            if ($d[10] =~ /^intr{swi\d+: netisr (\d+)}$/) {
+                my $wsid = $1;
+                my $pct = $d[9];
+                $pct =~ s/%$//;
+                $top{$wsid} = $pct;
+            }
+        }
+    }
+    return \%top;
+}
+
+
+sub collect_netisr {
     my $proto_filter = shift;
     open STAT, "netstat -Q |" or die "Can't spawn \"netstat -Q\": $!";
     my $got_stats;
@@ -47,32 +72,33 @@ if (@ARGV > 0) {
     }
 }
 
-my ($pdata, $data);
-$pdata = &collect(\@proto_filter);
-sleep $interval;
+my ($pnetisr, $ptop, $netisr);
+$pnetisr = &collect_netisr(\@proto_filter);
+$ptop = &collect_top($interval);
 my $ii = 1;
 while (1) {
-    $data = &collect(\@proto_filter);
-    printf "%8s %2s %5s %6s %8s %8s %8s %5s %8s\n",
-        "Name", "ID", "QLen", "WMark", "Handled", "Disp'd", "HDisp'd", "QDrops", "Queued";
-    for my $proto (sort keys %{$data}) {
-        for my $wsid (sort keys %{$data->{$proto}}) {
-            my $row = $data->{$proto}{$wsid};
-            my $prow = $pdata->{$proto}{$wsid};
-            printf "%8s %2d %5d %6d %8d %8d %8d %5d %8d\n",
+    $netisr = &collect_netisr(\@proto_filter);
+    printf "\n%8s %2s %5s %6s %8s %8s %8s %5s %8s %4s\n",
+        "Name", "ID", "QLen", "WMark", "Handled", "Disp'd", "HDisp'd", "QDrop", "Queued", "%CPU";
+    for my $proto (sort keys %{$netisr}) {
+        for my $wsid (sort keys %{$netisr->{$proto}}) {
+            my $row = $netisr->{$proto}{$wsid};
+            my $prow = $pnetisr->{$proto}{$wsid};
+            printf "%8s %2d %5d %6d %8d %8d %8d %5d %8d %4d\n",
                 $proto, $wsid, $row->{qlen}, $row->{wmark},
                 $row->{handled} - $prow->{handled},
                 $row->{dispd} - $prow->{dispd},
                 $row->{hdispd} - $prow->{hdispd},
                 $row->{qdrops} - $prow->{qdrops},
-                $row->{queued} - $prow->{queued};
+                $row->{queued} - $prow->{queued},
+                $ptop->{$wsid} || 0;
         }
     }
 
     last
         if $count and $ii == $count;
 
-    $pdata = $data;
-    sleep $interval;
+    $pnetisr = $netisr;
+    $ptop = &collect_top($interval);
     $ii++;
 }
