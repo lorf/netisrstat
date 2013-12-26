f36d746837f4b169cc21aedd55d88f2737d4dc86
Dmitry Frolov <dmitry.frolov@gmail.com>
2013-12-26 14:53:45 +0700

    Show totals

--- netisrstat.pl
+++ netisrstat.pl
@@ -18,10 +18,10 @@ _EOT_
     exit 1;
 }
 
-sub collect_proc {
+sub collect_cpu_stats {
     my $cmd = "ps Haxww -o %cpu,comm";
     open PROC, "$cmd |" or die "Can't spawn \"$cmd\": $!";
-    my %top;
+    my %cpu;
     my $ln = 0;
     while (<PROC>) {
         chomp;
@@ -29,21 +29,21 @@ sub collect_proc {
         if ($ln > 0) {  # Skip header
             my ($cpu, $cmd) = split /\s+/, $_, 2;
             if ($cmd and $cmd =~ /\bnetisr\s*(\d+)$/) {
-                $top{$1} = $cpu;
+                $cpu{$1} = $cpu;
             }
         }
         $ln++;
     }
     close PROC;
-    return \%top;
+    return \%cpu;
 }
 
 
-sub collect_netisr {
+sub collect_netisr_stats {
     my $proto_filter = shift;
     open STAT, "netstat -Q |" or die "Can't spawn \"netstat -Q\": $!";
     my $got_stats;
-    my %data;
+    my %stats;
     while (<STAT>) {
         chomp;
         s/^\s*//;
@@ -53,8 +53,8 @@ sub collect_netisr {
             my @row = split /\s+/, $_;
             next
                 if @{$proto_filter} and not grep { $row[2] eq $_ } @{$proto_filter};
-            $data{$row[2]}{$row[0]} = {
-                wsid    => $row[0],
+            $stats{$row[2]}{$row[0]} = {
+                id    => $row[0],
                 proto   => $row[2],
                 qlen    => $row[3],
                 wmark   => $row[4],
@@ -67,7 +67,7 @@ sub collect_netisr {
         }
     }
     close STAT;
-    return \%data;
+    return \%stats;
 }
 
 sub timer_handler {
@@ -77,78 +77,112 @@ sub timer_handler {
 
 sub print_stats {
     my $time = shift;
-    my $netisr = shift;
-    my $pnetisr = shift;
-    my $top = shift;
+    my $netisr_stats = shift;
+    my $prev_netisr_stats = shift;
+    my $cpu_stats = shift;
 
+    my %out = ();
+    my %out_proto_totals = ();
+    my %out_totals = ();
+    my $outproto = "-";
+    my $outdiv = $opts{S} ? 1 : $time;
+
+    for my $proto (sort keys %{$netisr_stats}) {
+        $outproto = $proto
+            if $opts{p};
+
+        for my $id (sort keys %{$netisr_stats->{$proto}}) {
+            my $row = $netisr_stats->{$proto}{$id};
+            my $prow = $prev_netisr_stats->{$proto}{$id};
+            $out{$outproto}{$id} = {}
+                unless defined $out{$outproto}{$id};
+            my $outrow = $out{$outproto}{$id};
+
+            $outrow->{cpu} = $cpu_stats->{$id} || 0;
+            $outrow->{qlen} += $row->{qlen};
+            $outrow->{wmark} += $row->{wmark};
+            $outrow->{handled} += ($row->{handled} - $prow->{handled}) / $outdiv;
+            $outrow->{dispd} += ($row->{dispd} - $prow->{dispd}) / $outdiv;
+            $outrow->{hdispd} += ($row->{hdispd} - $prow->{hdispd}) / $outdiv;
+            $outrow->{qdrops} += ($row->{qdrops} - $prow->{qdrops}) / $outdiv;
+            $outrow->{queued} += ($row->{queued} - $prow->{queued}) / $outdiv;
+
+            $out_proto_totals{$outproto}{cpu} += $cpu_stats->{$id} || 0;
+            $out_proto_totals{$outproto}{qlen} += $row->{qlen};
+            $out_proto_totals{$outproto}{wmark} += $row->{wmark};
+            $out_proto_totals{$outproto}{handled} += ($row->{handled} - $prow->{handled}) / $outdiv;
+            $out_proto_totals{$outproto}{dispd} += ($row->{dispd} - $prow->{dispd}) / $outdiv;
+            $out_proto_totals{$outproto}{hdispd} += ($row->{hdispd} - $prow->{hdispd}) / $outdiv;
+            $out_proto_totals{$outproto}{qdrops} += ($row->{qdrops} - $prow->{qdrops}) / $outdiv;
+            $out_proto_totals{$outproto}{queued} += ($row->{queued} - $prow->{queued}) / $outdiv;
+
+            $out_totals{qlen} += $row->{qlen};
+            $out_totals{wmark} += $row->{wmark};
+            $out_totals{handled} += ($row->{handled} - $prow->{handled}) / $outdiv;
+            $out_totals{dispd} += ($row->{dispd} - $prow->{dispd}) / $outdiv;
+            $out_totals{hdispd} += ($row->{hdispd} - $prow->{hdispd}) / $outdiv;
+            $out_totals{qdrops} += ($row->{qdrops} - $prow->{qdrops}) / $outdiv;
+            $out_totals{queued} += ($row->{queued} - $prow->{queued}) / $outdiv;
+        }
+        $out_totals{cpu} = $out_proto_totals{$outproto}{cpu}
+            unless $out_totals{cpu};
+    }
+
+    my (@hdr, $hfmt, $fmt);
     if ($opts{p}) {
-        printf "\n%6s %2s %4s %5s %6s %8s %8s %8s %5s %8s\n",
-            "Proto", "ID", "%CPU", "QLen", "WMark", "Handled", "Disp'd", "HDisp'd", "QDrop", "Queued";
-        for my $proto (sort keys %{$netisr}) {
-            for my $wsid (sort keys %{$netisr->{$proto}}) {
-                my $row = $netisr->{$proto}{$wsid};
-                my $prow = $pnetisr->{$proto}{$wsid};
-                my $fmt = "%6s %2d %4.1f %5d %6d %8d %8d %8d %5d %8d\n";
-                if ($opts{S}) {
-                    printf $fmt,
-                        $proto, $wsid, $top->{$wsid} || 0,
-                        $row->{qlen}, $row->{wmark},
-                        $row->{handled} - $prow->{handled},
-                        $row->{dispd} - $prow->{dispd},
-                        $row->{hdispd} - $prow->{hdispd},
-                        $row->{qdrops} - $prow->{qdrops},
-                        $row->{queued} - $prow->{queued};
-                } else {
-                    printf $fmt,
-                        $proto, $wsid, $top->{$wsid} || 0,
-                        $row->{qlen}, $row->{wmark},
-                        ($row->{handled} - $prow->{handled}) / $time,
-                        ($row->{dispd} - $prow->{dispd}) / $time,
-                        ($row->{hdispd} - $prow->{hdispd}) / $time,
-                        ($row->{qdrops} - $prow->{qdrops}) / $time,
-                        ($row->{queued} - $prow->{queued}) / $time;
-                }
+        @hdr = qw(Proto ID %CPU QLen WMark Handled Disp'd HDisp'd QDrop Queued);
+        $hfmt = "\n%6s %3s %5s %5s %6s %8s %8s %8s %5s %8s\n";
+        $fmt =    "%6s %3s %5.1f %5d %6d %8d %8d %8d %5d %8d\n";
+        printf $hfmt, @hdr;
+        for my $proto (sort keys %out) {
+            print "\n";
+            for my $id (sort keys %{$out{$proto}}) {
+                my $row = $out{$proto}{$id};
+                printf $fmt,
+                    $proto, $id,
+                    $row->{cpu}, $row->{qlen},
+                    $row->{wmark}, $row->{handled},
+                    $row->{dispd}, $row->{hdispd},
+                    $row->{qdrops}, $row->{queued};
             }
+            my $row = $out_proto_totals{$proto};
+            printf $fmt,
+                $proto, "Tot",
+                $row->{cpu}, $row->{qlen},
+                $row->{wmark}, $row->{handled},
+                $row->{dispd}, $row->{hdispd},
+                $row->{qdrops}, $row->{queued};
         }
+        print "\n";
+        my $row = \%out_totals;
+        printf $fmt,
+            "Total", "",
+            $row->{cpu}, $row->{qlen},
+            $row->{wmark}, $row->{handled},
+            $row->{dispd}, $row->{hdispd},
+            $row->{qdrops}, $row->{queued};
     } else {
-        my $ni = {};
-        for my $proto (sort keys %{$netisr}) {
-            for my $wsid (sort keys %{$netisr->{$proto}}) {
-                $ni->{$wsid} = {}
-                    unless defined $ni->{$wsid};
-                my $nrow = $ni->{$wsid};
-                my $row = $netisr->{$proto}{$wsid};
-                my $prow = $pnetisr->{$proto}{$wsid};
-                $nrow->{qlen} += $row->{qlen};
-                $nrow->{wmark} += $row->{wmark};
-                $nrow->{handled} += $row->{handled} - $prow->{handled};
-                $nrow->{dispd} += $row->{dispd} - $prow->{dispd};
-                $nrow->{hdispd} += $row->{hdispd} - $prow->{hdispd};
-                $nrow->{qdrops} += $row->{qdrops} - $prow->{qdrops};
-                $nrow->{queued} += $row->{queued} - $prow->{queued};
-            }
+        @hdr = qw(ID %CPU QLen WMark Handled Disp'd HDisp'd QDrop Queued);
+        $hfmt = "\n%3s %5s %5s %6s %8s %8s %8s %5s %8s\n";
+        $fmt =    "%3s %5.1f %5d %6d %8d %8d %8d %5d %8d\n";
+        printf $hfmt, @hdr;
+        my $proto = "-";
+        for my $id (sort keys %{$out{$proto}}) {
+            my $row = $out{$proto}{$id};
+            printf $fmt,
+                $id,
+                $row->{cpu}, $row->{qlen},
+                $row->{wmark}, $row->{handled},
+                $row->{dispd}, $row->{hdispd},
+                $row->{qdrops}, $row->{queued};
         }
-        printf "\n%2s %4s %5s %6s %8s %8s %8s %5s %8s\n",
-            "ID", "%CPU", "QLen", "WMark", "Handled", "Disp'd", "HDisp'd", "QDrop", "Queued";
-        for my $wsid (sort keys %{$ni}) {
-            my $nrow = $ni->{$wsid};
-            my $fmt = "%2d %4.1f %5d %6d %8d %8d %8d %5d %8d\n";
-            if ($opts{S}) {
-                printf $fmt,
-                    $wsid, $top->{$wsid} || 0,
-                    $nrow->{qlen}, $nrow->{wmark},
-                    $nrow->{handled}, $nrow->{dispd},
-                    $nrow->{hdispd}, $nrow->{qdrops},
-                    $nrow->{queued};
-            } else {
-                printf $fmt,
-                    $wsid, $top->{$wsid} || 0,
-                    $nrow->{qlen}, $nrow->{wmark},
-                    $nrow->{handled} / $time, $nrow->{dispd} / $time,
-                    $nrow->{hdispd} / $time, $nrow->{qdrops} / $time,
-                    $nrow->{queued} / $time;
-            }
-        }
+        my $row = \%out_totals;
+        printf $fmt,
+            "Tot",
+            $row->{cpu}, $row->{qlen},
+            $row->{wmark}, $row->{handled},
+            $row->{dispd}, $row->{hdispd},
+            $row->{qdrops}, $row->{queued};
     }
 }
 
@@ -171,7 +205,7 @@ $count = shift @ARGV
 {
     my $ii = 1;
     my $continue = 1;
-    my ($prev_netisr, $prev_timestamp);
+    my ($prev_netisr_stats, $prev_timestamp);
     local $SIG{'TERM'} = sub { $continue = 0 };
     local $SIG{'ALRM'} = \&timer_handler;
 
@@ -180,13 +214,13 @@ $count = shift @ARGV
     while ($continue) {
         if ($flag_collect_stats) {
             my $timestamp = time;
-            my $netisr = &collect_netisr(\@proto_filter);
-            my $top = &collect_proc();
+            my $netisr_stats = &collect_netisr_stats(\@proto_filter);
+            my $cpu_stats = &collect_cpu_stats();
 
-            if ($prev_netisr) {
-                &print_stats($timestamp - $prev_timestamp, $netisr, $prev_netisr, $top);
+            if ($prev_netisr_stats) {
+                &print_stats($timestamp - $prev_timestamp, $netisr_stats, $prev_netisr_stats, $cpu_stats);
             }
-            $prev_netisr = $netisr;
+            $prev_netisr_stats = $netisr_stats;
             $prev_timestamp = $timestamp;
 
             last
